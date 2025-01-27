0eb1942d454780acf04b3505f6aae89cfab07593
raysvarga <tyasrama123@gmail.com>
2025-01-27 08:37:44 +0700

    update banner

--- src/components/atoms/BannerGradient.jsx
+++ src/components/atoms/BannerGradient.jsx
@@ -3,7 +3,7 @@ import arsadaya from "../../assets/img/arsadaya-umkm.png";
 
 const BannerGradient = () => {
   return (
-    <div className="mt-5 bg-gradient-to-r from-[#5de0e6] to-[#004aad] rounded-2xl text-white py-6 ps-6 flex items-center justify-between">
+    <div className="mt-5 bg-gradient-to-r from-[#5de0e6] to-[#004aad] rounded-2xl text-white py-4 px-4 flex gap-3 items-center">
       <div className="shrink-0">
         <h3 className="text-2xl font-semibold leading-none">Arsadaya</h3>
         <p className="text-xs mt-3">
@@ -11,7 +11,7 @@ const BannerGradient = () => {
         </p>
       </div>
       <div>
-        <img src={arsadaya} alt="" className="w-32 mr-6" />
+        <img src={arsadaya} alt="" />
       </div>
     </div>
   );
